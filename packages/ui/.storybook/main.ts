e953c52a5aac7d8df952c49de1460d5e5896b58e
dklianev <mitko6754@gmail.com>
2026-05-23 14:25:40 +0300

    docs(ui): per-primitive MDX cheatsheets

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,8 +1,8 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(ts|tsx|mdx)"],
-  addons: ["@storybook/addon-a11y"],
+  stories: ["../src/**/*.stories.@(ts|tsx|mdx)", "../src/**/*.mdx"],
+  addons: ["@storybook/addon-docs", "@storybook/addon-a11y"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
