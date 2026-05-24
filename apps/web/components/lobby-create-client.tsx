9d9ec0d9ce1fabbc133821bc4e43b4e52eb29661
dklianev <mitko6754@gmail.com>
2026-05-24 23:52:17 +0300

    refactor(css): move manual role builder styles to module

--- apps/web/components/lobby-create-client.tsx
+++ apps/web/components/lobby-create-client.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import "@/components/lobby/LobbyWizard.module.css";
+import "@/components/ManualRoleBuilder.module.css";
 import type { GameFamily, GameMode } from "@werewolf/shared";
 import { LobbyWizard } from "@/components/lobby/LobbyWizard";
 import { roleThumbStyle } from "@/lib/role-art";
--- apps/web/components/manual-role-builder.tsx
+++ apps/web/components/manual-role-builder.tsx
@@ -2,6 +2,7 @@ import {
   ManualRoleBuilderClient,
   type ManualRoleBuilderClientProps,
 } from "@/components/manual-role-builder-client";
+import "@/components/ManualRoleBuilder.module.css";
 
 export type ManualRoleBuilderProps = ManualRoleBuilderClientProps;
 
